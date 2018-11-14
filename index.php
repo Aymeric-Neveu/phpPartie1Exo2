babe2f6cf1208f3d5a6c0d24d051326b7cb35890
root <emploi.neveu@gmail.com>
2018-11-14 16:13:08 +0100

    exo2

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP</title>
+  </head>
+  <body>
+    <?php
+    $name = "Rico";
+    $age = "18 ans";
+    $passion = "foot";
+    echo "Bonjour $name $age $passion";
+    ?>
+  </body>
+</html>
